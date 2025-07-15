b5c7f90cb0e178583a9bbf9edd38b4baf5ae7aa9
Ayush <119129362+Ayush-0404@users.noreply.github.com>
2025-07-15 23:52:39 +0530

    Delete backend directory

--- backend/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import PredictView, SessionPredictionsView
-
-urlpatterns = [
-    path('predict/', PredictView.as_view(), name='predict'),
-    path('session-predictions/', SessionPredictionsView.as_view(), name='session_predictions'),
-]
